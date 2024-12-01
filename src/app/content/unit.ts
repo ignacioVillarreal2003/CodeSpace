7b0f6a5823d7e1c72707d58e90338c8c39b7b83f
IgnacioVillarreal31 <ignacio.villarreal@correo.ucu.edu.uy>
2024-12-01 12:12:48 -0300

    i

--- src/app/content/unit.ts
+++ src/app/content/unit.ts
@@ -12,34 +12,41 @@ export const units: Unit[] = [
     topicId: "top-001",
     title: 'Introducción a TensorFlow',
     color: "#438ccf",
-    content: 'IA_and_Machine_Learning_for_Coders/Introduction to TensorFlow.md'
+    content: 'IA_and_Machine_Learning_for_Coders/Chapter_1.md'
   },
   {
     id: 'unit-002',
     topicId: "top-001",
     title: 'Introducción a la visión artificial',
     color: "#C2FFC7",
-    content: 'IA_and_Machine_Learning_for_Coders/Introduction to Computer Vision.md'
+    content: 'IA_and_Machine_Learning_for_Coders/Chapter_2.md'
   },
   {
     id: 'unit-003',
     topicId: "top-001",
     title: 'Más Allá de lo Básico: Detectando Características en Imágenes',
     color: "#C2FFC7",
-    content: 'IA_and_Machine_Learning_for_Coders/Going_Beyond_the_Basics_Detecting_Features_in_Images.md'
+    content: 'IA_and_Machine_Learning_for_Coders/Chapter_3.md'
   },
   {
     id: 'unit-004',
     topicId: "top-001",
     title: 'Usar conjuntos de datos públicos con TensorFlow Datasets',
     color: "#C2FFC7",
-    content: 'IA_and_Machine_Learning_for_Coders/Using_Public_Datasets_with_TensorFlow_Datasets.md'
+    content: 'IA_and_Machine_Learning_for_Coders/Chapter_4.md'
   },
   {
     id: 'unit-005',
     topicId: "top-001",
     title: 'Introducción al Procesamiento del Lenguaje Natural',
     color: "#C2FFC7",
-    content: 'IA_and_Machine_Learning_for_Coders/Introduction_to_Natural_Language_Processing.md'
+    content: 'IA_and_Machine_Learning_for_Coders/Chapter_5.md'
+  },
+  {
+    id: 'unit-006',
+    topicId: "top-001",
+    title: 'Haciendo que el sentimiento sea programable usando incrustaciones',
+    color: "#C2FFC7",
+    content: 'IA_and_Machine_Learning_for_Coders/Chapter_6.md'
   },
 ];
